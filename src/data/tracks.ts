dce7e6b71e20e6bf8ce5d96dc8c1b79c913e0da2
Asher Brook <2005.santhoshsivakumar@gmail.com>
2025-04-02 19:53:19 +0530

    initial commit

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -1,12 +1,20 @@
 // audio files
+import sit_still_look_pretty from './Daya - Sit Still, Look Pretty.mp3'
 import lean_on from './Lean On - Major Lazer, DJ Snake.mp3'
+import pretty_girls from './Reneé Rapp - Pretty Girls.mp3'
 import no_lie from './No Lie - Sean Paul, Dua Lipa.mp3';
 import levitating from './Levitating - Dua Lipa.mp3'
+import skin from './Sabrina Carpenter - Skin.mp3'
+import why from './Sabrina Carpenter - Why.mp3'
 
 // audio thumbnails
+import sit_still_look_pretty_thumbnail from './Daya - Sit Still, Look Pretty.jpg'
 import lean_on_thumbnail from './Lean On - Major Lazer, DJ Snake.jpg'
+import pretty_girls_thumbnail from './Reneé Rapp - Pretty Girls.jpg'
 import no_lie_thumbnail from './No Lie - Sean Paul, Dua Lipa.jpg';
 import levitating_thumbnail from './Levitating - Dua Lipa.jpg'
+import skin_thumbnail from './Sabrina Carpenter - Skin.jpg'
+import why_thumbnail from './Sabrina Carpenter - Why.jpg'
 
 // Data
 export const tracks = [
@@ -27,5 +35,29 @@ export const tracks = [
     src: lean_on,
     author: 'Major Lazer, DJ Snake',
     thumbnail: lean_on_thumbnail,
+  },
+  {
+    title: 'Daya - Sit Still, Look Pretty',
+    src: sit_still_look_pretty,
+    author: 'Daya',
+    thumbnail: sit_still_look_pretty_thumbnail,
+  },
+  {
+    title: 'Reneé Rapp - Pretty Girls',
+    src: pretty_girls,
+    author: 'Reneé Rapp',
+    thumbnail: pretty_girls_thumbnail,
+  },
+  {
+    title: 'Sabrina Carpenter - Skin',
+    src: skin,
+    author: 'Sabrina Carpenter',
+    thumbnail: skin_thumbnail,
+  },
+  {
+    title: 'Sabrina Carpenter - Why',
+    src: why,
+    author: 'Sabrina Carpenter',
+    thumbnail: why_thumbnail,
   },
 ];
